f2f691322f6f083afa6aa1367772a4fd291e572f
RustPlanet <adam.dudarov@hotmail.com>
2016-07-11 16:39:43 +0300

    Update serverInfo.js

--- js/serverInfo.js
+++ js/serverInfo.js
@@ -1,50 +1,15 @@
-<div ng-controller="ServerInfoController" flex layout="column" id="ServerInfoController">
+app.controller( 'ServerInfoController', ServerInfoController );
 
-	<div class="row">
+function ServerInfoController( $scope, rconService, $routeParams )
+{
+	$scope.info = {};
 
-		<div class="col-sm-12">
-			<h2 class="center">
-				{{info.Hostname}}
-				<div class="btn-group pull-right">
-					<button ng-click="refresh()" class="btn btn-default" type="button"><i class="glyphicon glyphicon-refresh"></i></button>
-				</div>
-			</h2>
-		</div>
+	$scope.refresh = function ()
+	{
+		rconService.Request( 'serverinfo', $scope, function ( msg ) {
+			$scope.info = JSON.parse( msg.Message );
+		});
+	}
 
-		<div class="col-sm-4" ng-repeat="(key, value) in info">
-			<p>{{key}}: <span class="badge">{{value}}</span></p>
-		</div>
-
-		<!--
-		<div class="col-sm-4">
-			<p>Map: {{info.Map}}</p>
-		</div>
-		<div class="col-sm-4">
-			<p>Max Players: {{info.MaxPlayers}}</p>
-		</div>
-		<div class="col-sm-4">
-			<p>GameTime: {{info.GameTime}}</p>
-		</div>
-		<div class="col-sm-4">
-			<p>Framerate: <span class="badge">{{info.Framerate}}</span></p>
-		</div>
-		<div class="col-sm-4">
-			<p>EntityCount: <span class="badge">{{info.EntityCount}}</span></p>
-		</div>
-		<div class="col-sm-4">
-			<p>Uptime: <span class="badge">{{info.Uptime}}</span></p>
-		</div>
-		<div class="col-sm-4">
-			<p>Players: <span class="badge">{{info.Players}}</span></p>
-		</div>
-		<div class="col-sm-4">
-			<p>Joining: <span class="badge">{{info.Joining}}</span></p>
-		</div>
-		<div class="col-sm-4">
-			<p>Queued: <span class="badge">{{info.Queued}}</span></p>
-		</div>
-		-->
-
-	</div>
-
-</div>
+	rconService.InstallService( $scope, $scope.refresh );
+}
